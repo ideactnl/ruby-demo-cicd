7a9c7ddb78230cfa5c69bd1132b09fdd778bedbd
larryfatt <60362139+larryfatt@users.noreply.github.com>
2025-11-11 12:46:15 +0100

    Production release feature/demo-pain-scale (#3)
    Add demo pain scale feature
    
    * Initial Flutter app structure
    
    * Add pubspec with version 1.0.8
    
    ---------
    
    Co-authored-by: larbifattah <larbi.fattah@pm.me>

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// Ruby App - Demo Version
+void main() {
+  runApp(RubyApp());
+}
+
+class RubyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ruby Menstrual Health',
+      home: HomeScreen(),
+    );
+  }
+}
+
+// Version: 1.0.8 - Demo
